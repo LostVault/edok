0bdef19af2295c3ee77d8a3dbd1c1f0d85cf22d1
beigbeider <beigbeider@gmail.com>
2021-08-07 14:36:10 +0300

    Вынесена Информация по аналогии с Wormhole

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 # ОСНОВНЫЕ
 from os import environ
 
-# Токен для авторизации тестового приложения:
+
+# Токен для авторизации приложения:
+# Тоокен генирируется автоматически при создание приложения на странице https://discord.com/developers/applications
 token = environ['EDOK_CLIENT_DISCORD']
 
-# Преффикс комманд:
-prefix = '>'
+# Имя файла база данных
+db_file_name = 'Edok.sqlite'
 
 # Краткое описание приложения:
-client_short_description = "E.D.O.K. — Elite Dangerous Outfitting Keeper"
+client_short_description = 'E.D.O.K. — Elite Dangerous Outfitting Keeper'
+
+# Полное описание приложения:
+client_full_description = 'E.D.O.K. — Elite Dangerous Outfitting Keeper'
 
 
 # ОСНОВЫНЕ // КОНЕЦ
