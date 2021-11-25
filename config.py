6b75840114539bc2297c6f1a4244dbaa5ac8e8b6
beigbeider <beigbeider@gmail.com>
2021-11-25 21:49:49 +0300

    Переписанный конфиг под новую код.

--- config.py
+++ config.py
@@ -1,21 +1,28 @@
-# ОСНОВНЫЕ
-from os import environ
-
+# region •••••••••••••••• ОСНОВНЫЕ НАСТРОЙКИ
+from os import environ  # Импортируем библиотеку для работы с переменными
 
 # Токен для авторизации приложения:
 # Тоокен генирируется автоматически при создание приложения на странице https://discord.com/developers/applications
-token = environ['EDOK_CLIENT_DISCORD']
+token = environ['APP_TOKEN']
 
-# Имя файла база данных
-db_file_name = 'Edok.sqlite'
+# Тип окружения, в котором производится запуск
+environment_type = environ['APP_ENVIRONMENT'].lower()
+assert environment_type in ['test', 'prod'], f"Wrong environment type: {environment_type}, must be 'test' or 'prod'"
 
+# endregion ••••••••••••• ОСНОВНЫЕ НАСТРОЙКИ // КОНЕЦ
+
+
+# region •••••••••••••••• ОСНОВНАЯ ИНФОРМАЦИЯ
 # Краткое описание приложения:
-client_short_description = 'E.D.O.K. — Elite Dangerous Outfitting Keeper'
+app_short_description = 'E.D.O.K. — Elite Dangerous Outfitting Keeper'
 
 # Полное описание приложения:
-client_full_description = 'E.D.O.K. — Elite Dangerous Outfitting Keeper'
+app_full_description = 'Приложение для вывода списка с ссылками на сборки кораблей с сервиса Coriolis EDCD Edition.'
 
+# Приглашение на сервер поддержки
+app_support_server_invite = 'https://discord.gg/HFqmXPvMxC'
 
-# ОСНОВЫНЕ // КОНЕЦ
+# Ссылка на репозиторий на GitHub
+app_github_url = 'https://github.com/LostVault/edok'
 
-
+# endregion ••••••••••••• ОСНОВНАЯ ИНФОРМАЦИЯ // КОНЕЦ
